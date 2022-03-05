d58fe62796b61b8a34366ade64113a7b54d96886
MoonLGH <moonlmc1212@gmail.com>
2022-03-05 19:13:24 +0700

    akua

--- .github/scripts/commit-repo.sh
+++ .github/scripts/commit-repo.sh
@@ -5,7 +5,10 @@ rsync -a --delete --exclude .git --exclude .gitignore ../repo/ .
 git config --global user.email "github-actions[bot]@users.noreply.github.com"
 git config --global user.name "github-actions[bot]"
 git status
-git add .
-git commit -m "Update extensions repo"
-git push
-echo "No changes to commit"
+if [ -n "$(git status --porcelain)" ]; then
+    git add .
+    git commit -m "Update extensions repo"
+    git push
+else
+    echo "No changes to commit"
+fi
