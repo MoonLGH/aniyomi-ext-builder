d28eba76f8e9278e4c7bb103c931dad2a7717ea7
MoonLGH <moonlmc1212@gmail.com>
2022-03-05 18:50:30 +0700

    iasda

--- .github/scripts/commit-repo.sh
+++ .github/scripts/commit-repo.sh
@@ -5,8 +5,7 @@ rsync -a --delete --exclude .git --exclude .gitignore ./repo/ .
 git config --global user.email "github-actions[bot]@users.noreply.github.com"
 git config --global user.name "github-actions[bot]"
 git status
-
-cd repo
 git add .
-git commit -m "Updated Extensions"
+git commit -m "Update extensions repo"
 git push
+echo "No changes to commit"
